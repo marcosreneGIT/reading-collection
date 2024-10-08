0df607e9c087904d35ff9621cb9b704633e4f6df
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-10-08 20:16:11 -0300

    Update pesquisa_binaria.py

--- exemplos/pesquisa_binaria.py
+++ exemplos/pesquisa_binaria.py
@@ -19,7 +19,7 @@ def pesquisa_binaria(lista, item): # lista precisa esta ordenada
 
 lista = [0, 1, 3, 4, 7, 8, 10, 15, 16, 18, 19]
 
-print(pesquisa_binaria(lista, 8))
-print(lista[pesquisa_binaria(lista, 8)])
+print(pesquisa_binaria(lista, 15))
+print(lista[pesquisa_binaria(lista, 15)])
 
         
